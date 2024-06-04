c968b1882541655ed06ce83b2a6cc6c7d29c2abe
accoffin12 <accoffin@outlook.com>
2024-06-04 18:32:50 -0500

    Struct Library added to ProducerV2 and successfull log of run.

--- temp_ProducerV2.py
+++ temp_ProducerV2.py
@@ -5,6 +5,7 @@
 
    Program will read in temepratures provided in 'smoker-temps.csv'.
    Will send temperatures to respective consumers based on what we are reading the temperature of (smoker, food_A, food_B).
+   Addition of Struct library to use C structures during the process.
 
    Author: Alex Coffin
    Date: June, 2024
@@ -20,6 +21,7 @@ from datetime import datetime
 import pika.exceptions
 from utils.util_logger import setup_logger
 import time
+import struct
 
 # Configuring the Logger:
 logger, logname = setup_logger(__file__)
@@ -91,25 +93,39 @@ try:
         next(reader)
         # reading rows from csv
         for row in reader:
-            timestamp = row[0]
+            timestamp_str = row[0]
             smoker_temp = row[1]
             food_A_temp = row[2]
             food_B_temp = row[3]
 
-            # Created for smoker_temp, using encode which encodes the data as a binary output
+            # Convert dates into Unix date timestamp that can be added to struct later.
+            timestamp = datetime.strptime(timestamp_str, "%m/%d/%y %H:%M:%S").timestamp()
+
+            """
+            Created for smoker_temp, using struct which conversts python values and C structs represented as Python bytes.
+            This is done to compat formate strings that will be converted to and from Python Values.
+            Format strings descirbe the data layout when packing and unpacking the data, 
+            this is controled by format characters that specifiy the type of data being pack/unpacked. 
+            This process heavily relies on the use of C types in the machines native format and byte order.
+
+            Original segments of code prior to adding struct are commented out to show developement.
+            """
             if smoker_temp:
-                smoker_temp = float(smoker_temp)
-                message = (timestamp, smoker_temp)
+                #smoker_temp = float(smoker_temp)
+                #message = (timestamp, smoker_temp)
+                message = struct.pack('!df', timestamp, float(smoker_temp))
                 send_message(host, "01-smoker", message)
                 logger.info(f"[x] Sent Message to {host} in '01-smoker', {message}")
             # Created message for food_A_temp
             if food_A_temp:
-                message = (timestamp, food_A_temp)
+                #message = (timestamp, food_A_temp)
+                message = struct.pack('!df', timestamp, float(food_A_temp))
                 send_message(host, "02-food-A", message)
                 logger.info(f"[x] Sent Message to {host} in '02-food-B', {message}")
                 # Created a message for food_B_temp
             if food_B_temp:
-                message = (timestamp, food_B_temp)
+                #message = (timestamp, food_B_temp)
+                message = struct.pack('!df', timestamp, float(food_B_temp))
                 send_message(host, "03-food-B", message)
                 logger.info(f"[x] Sent Message to {host} in '03-food-B', {message}")
                 # Set sleep for 30 seconds before reading the next row:
